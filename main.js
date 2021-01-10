92479b1ae68a0fc8c69451c487ddc41753715be6
Liheng <lihengcao@gmail.com>
2021-01-09 21:25:19 -0600

    colors, centering
    updated the colors for the light and dark squares on the board, and made the entire thing centered

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-// export {}; // make ts and js have different "scopes"
 console.log('chessVariants');
 var canvas = document.getElementById('board');
 var context = canvas.getContext('2d');
 // console.log(canvas);
+var board_light = '#FFCE9E';
+var board_dark = '#D18B47';
 // draw squares, board background
 for (var r = 0; r < 8; ++r) {
     for (var c = 0; c < 8; ++c) {
-        context.fillStyle = ((r + c) & 1) ? '#123456' : '#654321';
+        context.fillStyle = ((r + c) & 1) ? board_dark : board_light;
         context.fillRect(r * 80, c * 80, (r + 1) * 80, (c + 1) * 80);
     }
 }
--- main.ts
+++ main.ts
@@ -4,11 +4,12 @@ const canvas = document.getElementById('board') as HTMLCanvasElement;
 const context = canvas.getContext('2d');
 
 // console.log(canvas);
-
+let board_light = '#FFCE9E'
+let board_dark = '#D18B47'
 // draw squares, board background
 for (let r = 0; r < 8; ++r){
     for (let c = 0; c < 8; ++c){
-        context.fillStyle = ((r+c) & 1) ? '#123456' : '#654321';
+        context.fillStyle = ((r+c) & 1) ? board_dark : board_light;
         context.fillRect(r*80, c*80, (r+1)*80, (c+1)*80);
         
     }
