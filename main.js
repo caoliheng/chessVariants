f86380118be3f28ebd29dbc6728b9818f3bb031f
Liheng <lihengcao@gmail.com>
2021-01-10 20:57:04 -0600

    changed to react

--- main.js
+++
@@ -1,3 +0,0 @@
-console.log('chessVariants');
-var board_light = '#FFCE9E';
-var board_dark = '#D18B47';
--- main.ts
+++
@@ -1,4 +0,0 @@
-console.log('chessVariants');
-
-let board_light = '#FFCE9E'
-let board_dark = '#D18B47'
